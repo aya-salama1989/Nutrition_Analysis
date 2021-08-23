f2dc1e73b8cb2911ca4e6e163911eb4b54683c80
ayasalama <ayoyasalama@gmail.com>
2021-08-23 19:00:30 +0200

    dummy tests

--- app/src/test/java/com/misr/nutritionanalysis/core/extensions/MathExtensionsKtTest.kt
+++ app/src/test/java/com/misr/nutritionanalysis/core/extensions/MathExtensionsKtTest.kt
@@ -1,11 +1,11 @@
 package com.misr.nutritionanalysis.core.extensions
 
+import org.junit.Assert.*
 import org.junit.Test
 
-class MathExtensionsTest {
-
+class MathExtensionsKtTest{
     @Test
     fun isDoubleNumberRoundedToTwoDecimals() {
-       assert(35.0000434.roundTo(2).toString().length == 4)
+        assert(35.0000434.roundTo(2).toString().length == 4)
     }
 }
