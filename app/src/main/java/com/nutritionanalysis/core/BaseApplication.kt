40fc49b8d05b0c049b0900d42ee91547deec0094
Aya Salama <108944018+ayasalama54@users.noreply.github.com>
2024-10-01 13:54:46 +0300

    fixing test case

--- app/src/main/java/com/nutritionanalysis/core/BaseApplication.kt
+++ app/src/main/java/com/nutritionanalysis/core/BaseApplication.kt
@@ -1,12 +1,15 @@
 package com.nutritionanalysis.core
 
+import com.nutritionanalysis.core.di.component.ApplicationComponent
 import com.nutritionanalysis.core.di.component.DaggerApplicationComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
 class BaseApplication: DaggerApplication() {
 
+    val appComponent: ApplicationComponent = DaggerApplicationComponent.builder().application(this).build()
+
     override fun applicationInjector(): AndroidInjector<out DaggerApplication>{
-       return DaggerApplicationComponent.builder().application(this).build()
+        return appComponent
     }
 }
--- app/src/main/java/com/nutritionanalysis/modules/summary/SummaryViewModel.kt
+++ app/src/main/java/com/nutritionanalysis/modules/summary/SummaryViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.nutritionanalysis.modules.analyse.presentation.NutrientsList
 import javax.inject.Inject
 
-class SummaryViewModel @Inject constructor() : ViewModel() {
+open class SummaryViewModel @Inject constructor() : ViewModel() {
 
     fun countTotalCalories(ingredients: NutrientsList): Double {
         var total = 0.0
